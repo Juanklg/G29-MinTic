2d4e6aafbc45640f66126e2a97cf940276f5519d
DESKTOP-QI3AGC3\Juan <juanklg25@gmail.com>
2021-08-09 19:51:23 -0500

    Add spyder document

---
+++ Scripts/ParaDoc/11-Spyder.py
@@ -0,0 +1,13 @@
+#Primero tenemos q crear el ambiente e instalar con conda
+"""
+conda create -n spyder-env spyder=4 numpy scipy pandas matplotlib sympy cython
+"""
+
+#Segundo activamos el env
+"""
+conda activate spyder-env
+"""
+#luego tipeamos 
+"""
+spyder
+"""
