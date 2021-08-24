9c49d4ab962a29d84712923394861dd9a7cf5de1
Juan Linares <juanklg25@gmail.com>
2021-08-24 18:13:49 -0500

    Add List, update index

---
+++ Scripts/ParaDoc/15-List.py
@@ -0,0 +1,88 @@
+#List
+
+def buscarYeliminar(lista,parametro):
+    pos = lista.index(parametro)
+    lista.pop(pos)
+    return lista
+
+#comparacion de estructuras de datos
+lista = ['Juan','Karla','Ricardo', 'María']
+tupla = ('Naranja', 'Plátano', 'Guayaba')
+conjunto = {'Marte', 'Júpiter', 'Venus'}
+
+#imprimir elementos
+detailArch(lista)
+detailArch(tupla)
+# detailArch(conjunto)
+
+#el conjunto no permite indexacion
+print(lista[1])
+print(lista[-1])
+print(tupla[1])
+print(tupla[-1])
+
+#validar si existe dentro de la estructura
+print('Karla' in lista)
+print('Plátano' in tupla)
+print('Marte' in conjunto)
+
+#verificar metodos comunes
+detailArch(lista)
+lista.append('Ashley')  # append
+lista.insert(1,'juan')  # insert
+print('Cuantas veces esta juan =',lista.count('Juan')) # count
+
+grupo29 = ( # Tupla de estudiantes
+    'Ana',
+    'Dayana',
+    'Juan D',
+    'Kevin',
+    'Mateo',
+    'Paulina',
+    'Samuel',
+    'Santiago',
+    'Tomas'
+)
+detailArch(grupo29)
+print('Buscando index en pos :',grupo29.index('Paulina'))
+print('Count con la tupla : ',grupo29.count('Kevin')) # count
+
+#convertimos la tupla del grupo 29 en una lista
+ListGrupo29 = list(grupo29)
+
+detailArch(ListGrupo29)
+#lista orignal de estudiantes
+detailArch(lista)
+#la lista original le agrega la informacion de la lista del grupo 29
+lista.extend(ListGrupo29)#extend no solo funciona con listas sino tambien con tuplas
+detailArch(lista)
+# Antes de organizar la lista la invertimos manteniendo su posicion de ingreso
+lista.reverse()
+detailArch(lista)
+#la lista la vamos a organizar
+lista.sort(reverse=True)
+detailArch(lista)
+lista.sort()
+detailArch(lista)
+
+# ahora usamos el'remove' que elimina el primer elemento q encuentra con ese nombre
+lista.remove('Juan')
+detailArch(lista)
+#Buscamos a Juan para sacarlo de la lista
+posJuan = lista.index('juan')
+print('Donde esta Juan',posJuan)
+#aplicar el pop y elminar el ultimo de la lista si sepa alimpio o el indice se se pasa
+lista.pop(posJuan)
+detailArch(lista)
+#creamis una func de las ultimas lineas para buscar y eliminar a un integrante 
+lista=buscarYeliminar(lista,'Karla')
+lista=buscarYeliminar(lista,'María')
+detailArch(lista)
+
+print(dir(lista))
+# clear
+lista.clear()
+detailArch(lista)
+
+del lista
+print(lista)
--- index.py
+++ index.py
@@ -3,8 +3,17 @@
 #Variables (int,float,str,bool)
 #iterables o colecciones = {range,list,tuple,dict,set}
 
-#funciones -> Fun.propias, Fun.python
-#bucles (while,for)
+#funciones -> {Fun.propias, Fun.python, Fun,Clase}
+#Sentencias de control = {if, for, while}
+
+def detailArch(el):
+    print('---------------------------------')
+    print(el)
+    print("len = ",len(el))
+    for x in range(len(el)):
+        print(f"Pos : {x} - Elemento : {el[x]}")
+    # for x in el:
+    #     print(f"Elemento : {x}")
 
 def detailVar(var:any):
     print("El valor es :",var)
@@ -28,113 +37,83 @@ isLogin(user)
 ## solo se ejecuta esto si el usuario es = 'Login'
 print("Inicia la App")
 
-def detailArch(el):
-    print('---------------------------------')
-    print(el)
-    print("len = ",len(el))
-    for x in range(len(el)):
-        print(f"Pos : {x} - Elemento : {el[x]}")
-    # for x in el:
-    #     print(f"Elemento : {x}")
-
-def buscarYeliminar(lista,parametro):
-    pos = lista.index(parametro)
-    lista.pop(pos)
-    return lista
-
-#comparacion de estructuras de datos
-lista = ['Juan','Karla','Ricardo', 'María']
-tupla = ('Naranja', 'Plátano', 'Guayaba')
-conjunto = {'Marte', 'Júpiter', 'Venus'}
-
-#imprimir elementos
-# detailArch(lista)
-# detailArch(tupla)
-# detailArch(conjunto)
-
-#el conjunto no permite indexacion
-# print(lista[1])
-# print(lista[-1])
-# print(tupla[1])
-# print(tupla[-1])
-
-#validar si existe dentro de la estructura
-# print('Karla' in lista)
-# print('Plátano' in tupla)
-# print('Marte' in conjunto)
-
-#verificar metodos comunes
-detailArch(lista)
-lista.append('Ashley')  # append
-lista.insert(1,'Juan')  # insert
-print('Cuantas veces esta juan =',lista.count('Juan')) # count
-
-grupo29 = (
-    'Ana',
-    'Dayana',
-    'Juan D',
-    'Kevin',
-    'Mateo',
-    'Paulina',
-    'Samuel',
-    'Santiago',
-    'Tomas'
-)
-detailArch(grupo29)
-print('Buscando index en pos :',grupo29.index('Paulina'))
-print('Count con la tupla : ',grupo29.count('Kevin')) # count
-
-#convertimos a tupla del grupo 29 en una lista
-ListGrupo29 = list(grupo29)
-
-detailArch(ListGrupo29)
-#lista orignal de estudiantes
-detailArch(lista)
-#la lista original le agrega la informacion de la lista del grupo 29
-lista.extend(ListGrupo29)#extend
-detailArch(lista)
-# Antes de organizar la lista la invertimos manteniendo su posicion de ingreso
-lista.reverse()
-detailArch(lista)
-#la lista la vamos a organizar
-lista.sort(reverse=True)
-detailArch(lista)
-lista.sort()
-detailArch(lista)
-
-# ahora usamos el'remove' que elimina el primer elemento q encuentra con ese nombre
-lista.remove('Juan')
-detailArch(lista)
-#Buscamos a Juan para sacarlo de la lista
-posJuan = lista.index('Juan')
-print('Donde esta Juan',posJuan)
-#aplicar el pop y elminar el ultimo de la lista si sepa alimpio o el indice se se pasa
-lista.pop(posJuan)
-detailArch(lista)
-#usamos una funcion para buscar y elominar
-lista=buscarYeliminar(lista,'Karla')
-lista=buscarYeliminar(lista,'María')
-detailArch(lista)
-# clear
-lista.clear()
-detailArch(lista)
+Estudiantes = ['Ana', 'Ashley', 'Dayana', 'Juan D', 'Kevin', 'Mateo', 'Paulina', 'Ricardo', 'Samuel', 'Santiago', 'Tomas', 'Yaser']
 
 #con al tupla puedo captura los elementos con un desembalaje
 fruits = ("apple", "banana",  "cherry", "strawberry", "raspberry")
 print(fruits)
 
-(princ, *secun, penultimo,ultimo) = fruits
+#concatenar con signo +
+t = ("pera",)
+nt = fruits+t
+print("Concat whit (+)",nt)
 
-print(princ)
-print(secun)
-print(penultimo)
-print(ultimo)
+#Patron DSU -> organiza las tuplas segu un parametro
+TupleEst = tuple(Estudiantes)
+print(TupleEst)
+#Decorate
+listaDecorate = list()
+for est in TupleEst:
+    listaDecorate.append((len(est),est))
+#Sort
+print(listaDecorate.sort())
+#undecorate
+listaSortLen = list()
+for lgt,name in listaDecorate:
+    print(name)
+    listaSortLen.append(name)
 
-thislist = ["apple", "banana", "cherry"]
-thislist[1:3] = ["watermelon"]
-print(thislist)
+print(listaSortLen)
+
+#desembalaje o umpack
+a,*b,c = listaSortLen
+# print(a)
+# print(b)
+# print(c)
+#el desembalaje se hace mejor sobre una tupla
+props = ("nombre","edad","grupo","proyecto","username")
+(nombre, edad, grupo,proyecto,username) = props
+
+print(nombre)
+print(edad)
+
+#intercambiar valores entre variables
+nombre,edad=edad,nombre
+
+print(nombre)
+print(edad)
+
+#uso de la asignacion multiple en una linea
+email = "jkltsd@python.org"
+user,domain = email.split('@')
+
+print(user)
+print(domain)
+
+
+
+# crear un diccionario de la lista de estudiantes
+
+dicEstudiantes = dict.fromkeys(listaSortLen)
+print(dicEstudiantes)
+
+lTuples = dicEstudiantes.items()
+
+#lista de tu´plas con valores genericos
+nuevaListaTuple = list()
+for est,x2 in dicEstudiantes.items():
+    nuevaListaTuple.append((est,29,15))
+
+print(nuevaListaTuple)
+
+# crear un diccionario con tuplas como llaves
+dicFinal = dict()
+for key,grupo,edad, in nuevaListaTuple:
+    dicFinal[(key,grupo)] = 15
+
+print(dicFinal)
+detailArch(nuevaListaTuple)
+# print(dir(dicEstudiantes))
+
 
 # 'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update'
-
-
-
