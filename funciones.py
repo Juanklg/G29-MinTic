d5f49dc830a7df9290922cce1d8bc464ddf7b334
Juan Linares <juanklg25@gmail.com>
2021-08-26 18:26:51 -0500

    Add dbxls, functions and menuapp

--- funciones.py
+++ funciones.py
@@ -1,8 +1,8 @@
+import crud
+from datetime import datetime
 #Primer script que corre mi aplicacion
-
 #Variables (int,float,str,bool)
 #iterables o colecciones = {range,list,tuple,dict,set}
-
 #funciones -> {Fun.propias, Fun.python, Fun,Clase}
 #Sentencias de control = {if, for, while}
 
@@ -46,3 +46,114 @@ def printMenu(el):
     print('---------------------------------')    
     for i,e in zip(range(1,len(el)+1),el):
         print(f"\t{i} - {e}")
+
+def menuOriginal(rut):
+    datosActualizados = {
+        'titulo':'',
+        'descripcion':'',
+        'estado':'',
+        'fechaInicio':'',
+        'fechaFinalizacion':'',
+    }
+    while True:
+        print('Indique la accion que desea realizar: ')
+        print('\t 1-Consultar')
+        print('\t 2-Actualizar')
+        print('\t 3-Crear nueva tarea')
+        print('\t 4-Borrar')
+        accion = input('Escriba la opccion: ')
+
+        if not(accion=="1") and not(accion=="2") and not(accion=="3") and not(accion=="4"):
+            print('Comando invalido por favor eliga una opcion valida')
+        elif accion=='1': 
+            opc_consulta='' 
+            print('Indique las tareas que desea consultar: ') 
+            print('\t 1-Todas las tareas') 
+            print('\t 2-En espera') 
+            print('\t 3-En ejecucion') 
+            print('\t 4-Por aprobar') 
+            print('\t 5-Finalizada') 
+            opc_consulta = input('Digite las tareas que desea consultar: ')
+            print("Consulta",opc_consulta)
+            if opc_consulta=='1': 
+                print() 
+                print() 
+                print('* Consultando todas Las tareas *') 
+                crud.leer(rut, 'todo') 
+            elif opc_consulta=='2': 
+                print() 
+                print() 
+                print('* Consultando tareas en espera *') 
+                crud.leer(rut, 'En espera') 
+            elif opc_consulta=='3': 
+                print() 
+                print() 
+                print('* Consultando tareas en ejecucion *') 
+                crud.leer(rut, 'En ejecucion') 
+            elif opc_consulta=='4': 
+                print() 
+                print() 
+                print('* Consultando tareas por aprobar *') 
+                crud.leer(rut, 'Por aprobar') 
+            elif opc_consulta=='5': 
+                print() 
+                print() 
+                print('* Consultando tareas finalizadas *') 
+                crud.leer(rut, 'Finalizada')
+        elif accion=='2': 
+            datosActualizados={'titulo':'', 'descripcion':'', 'estado':'', 'fecha inicio':'', 'fecha finalizacion':''}
+            print('* Actualizar Tarea *') 
+            print() 
+            id_Actualizar=int(input('Indique el Id de la tarea que desea actualizar: ')) 
+            print() 
+            print('* Nuevo titulo *') 
+            print('*Nota: si no desea actualizar el titulo solo oprima ENTER') 
+            datosActualizados['titulo']=input('Indique el nuevo titulo de la tarea : ') 
+            print() 
+            print('* Nueva descripcion *') 
+            print('Nota: si no desea actualizar la descripcion solo oprima ENTER') 
+            datosActualizados[ 'descripcion']= input('Indique La nueva descripcion de la tarea : ') 
+            print() 
+            print('* Nueva estado **') 
+            print('En espera: 2') 
+            print('En ejecucion: 3') 
+            print('Por aprobar 4') 
+            print('Finalizada: 5')
+            print('**Nota: si no desea actualizar el estado solo oprima ENTER') 
+            estadoNuevo= input('Indique el nuevo estado de la tarea : ') 
+            if estadoNuevo=='2': 
+                datosActualizados['estado']='En espera' 
+            elif estadoNuevo=='3': 
+                datosActualizados[ 'estado']='En ejecucion' 
+            elif estadoNuevo=='4': 
+                datosActualizados[ 'estado']='Por aprobar' 
+            elif estadoNuevo=='5': 
+                now = datetime.now() 
+                datosActualizados['estado']='Finalizada' 
+                datosActualizados['fecha finalizacion']=str(now.day) +'/'+ str(now.month) +'/'+str(now.year) 
+                
+            now = datetime.now() 
+            datosActualizados['fecha inicio']=str(now.day) +'/'+ str(now.month) +'/'+str(now.year) 
+            crud.actualizar(rut,id_Actualizar, datosActualizados) 
+            print()
+        elif accion=='3': 
+            datosActualizados={'tarea':'', 'descripcion':", 'estado':'", 'fecha inicio':'', 'fecha finalizacion':''} 
+            print('* Crear nueva Tarea *') 
+            print() 
+            print('* titulo *') 
+            print() 
+            datosActualizados['titulo']=input('Indique el titulo de la tarea : ')
+            print() 
+            print('* descripcion *') 
+            datosActualizados['descripcion']= input('Indique la descripcion de la tarea : ')
+            print() 
+            datosActualizados['estado']='En espera' 
+            now = datetime.now() 
+            datosActualizados['fecha inicio']=str(now.day) +'/'+ str(now.month) +'/'+str(now.year) 
+            datosActualizados['fecha finalizacion']=''
+            crud.agregar(rut, datosActualizados) 
+        elif accion=='4': 
+            print('') 
+            print('* ELiminar Tarea **') 
+            iden=int(input('Indique el Id de la tarea que desea eliminar: '))
+            crud.borrar(rut, iden)
--- index.py
+++ index.py
@@ -1,14 +1,98 @@
 # import funciones
 # print(dir(funciones))
-from funciones import isLogin, detailArch, detailArchZip
-import crud as cd
+from funciones import isLogin, menuOriginal
+# from funciones as fn
 
-user = "Login"
-isLogin(user)
-#------------------------------------------------------    
+import crud
+
+isLogin("Login")
+#------------------------------------------------------
 ## solo se ejecuta esto si el usuario es = 'Login'
 print("Inicia la App")
 
-cd.Read()
+rut = r"C:\Users\MakeDream\Desktop\Ruta1\G29-MinTic\dbTask.xlsx"
+menuOriginal(rut)
 
 
+
+
+
+
+
+
+# task = {"Nombre","Descripcion","Estado","Inicio","Fin"}
+# opciones = ("Create","Read","Update","Delete","Exit")
+# filterState = ["Todas","Pendientes","Ejecucion","Revision","Finalizada"]
+# menuState = filterState[1:]
+
+# while True:
+#     opcion=''
+#     print('---------------------------------')
+#     printMenu(opciones)
+#     opcion=input('Digite la opcion deseada: ')
+#     print("\nUsted selecciono la opcion",opcion)
+#     if not(opcion=="1") and not(opcion=="2") and not(opcion=="3") and not(opcion=="4") and not(opcion=="5"):
+#         print("Comando Invalido")
+#     elif opcion=="1":#Create
+#         dictTask=dict.fromkeys(task)        
+#         print('\n* Crear nueva Tarea *')
+
+#         print('\n* Nombre *') 
+#         dictTask['Nombre']=input('Indique el Nombre de la tarea : ')
+
+#         print('\n* Descripcion *') 
+#         dictTask['Descripcion']= input('Indique la descripcion de la tarea : ')
+
+#         dictTask['Estado']='Pendientes'
+#         now = datetime.now()
+#         dictTask['Inicio']=str(now.day) +'/'+ str(now.month) +'/'+str(now.year)
+#         dictTask['Finalizada']=''
+#         crud.Create(rut, dictTask)
+#     elif opcion=="2":#Read
+#         printMenu(filterState)
+#         opcion=input('\nIndique la lista de tareas que quiere revisar: ')
+#         print("\nUsted selecciono la opcion",opcion)
+#         if not(opcion=="1") and not(opcion=="2") and not(opcion=="3") and not(opcion=="4"):
+#             print("Comando Invalido")
+#         elif opcion=="1":
+#             print('* Consultando Todas Las tareas*') 
+#             crud.Read(rut, 'Todas')
+#         elif opcion=="2":
+#             print('* Consultando las tareas Pendientes*')
+#             crud.Read(rut, 'Pendientes')
+#         elif opcion=="3":
+#             print('* Consultando las tareas Ejecucion*')
+#             crud.Read(rut, 'Ejecucion')
+#         elif opcion=="4":
+#             print('* Consultando las tareas Revision*')
+#             crud.Read(rut, 'Revision')
+#         elif opcion=="5":
+#             print('* Consultando las tareas Finalizada*')
+#             crud.Read(rut, 'Finalizada')
+#     elif opcion=="3":#update
+#         dictTask=dict.fromkeys(task)
+#         print('\n* Actualizar Tarea *')
+#         id=int(input('Indique el Id de la tarea que desea actualizar: '))
+
+#         print('\n* Nuevo titulo *')
+#         print('*Nota: si no desea actualizar el titulo solo oprima ENTER')
+#         dictTask['Nombre']=input('Indique el nuevo titulo de la tarea : ')
+        
+#         print('\n* Nuevo Descripcion *')
+#         print('*Nota: si no desea actualizar el titulo solo oprima ENTER')
+#         dictTask['Descripcion']=input('Indique la descripcion de la tarea : ')
+
+#         print('\n* Nuevo Estado *')
+#         printMenu(menuState)
+#         print('*Nota: si no desea actualizar el titulo solo oprima ENTER')        
+#         newState = int(input('Indique el nuevo estado de la  tarea : '))
+#         dictTask['Estado'] = filterState[newState]
+#         if newState == 4:
+#             now = datetime.now()
+#             dictTask['Fin'] = str(now.day) +'/'+ str(now.month) +'/'+str(now.year)
+#         crud.Update(rut,id,dictTask)
+#     elif opcion=="4":#delete        
+#         print('* ELiminar Tarea **') 
+#         id=int(input('Indique el Id de la tarea que desea eliminar: '))
+#         crud.Delete(rut, id)
+#     elif opcion=="5":exit()
