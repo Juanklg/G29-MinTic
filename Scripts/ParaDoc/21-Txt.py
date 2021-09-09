b5c800775236de2fd716b98cad46e99f6a1365aa
Juan Linares <juanlinares.ruta1@utp.edu.co>
2021-09-09 18:35:22 -0500

    update txt, reto3 data task github

--- Scripts/ParaDoc/21-Txt.py
+++ Scripts/ParaDoc/21-Txt.py
@@ -1,43 +1,34 @@
 print("Txt".center(60,'-'))
 
-ruta = r'C:\Users\MakeDream\Desktop\Ruta1\G29-MinTic\DataBase\\'
+ruta = r'C:\Users\MakeDream\Desktop\Ruta1\G29-MinTic\DataBase\txt\\'
 
+def leerTxt(archivo:str):
+    print(f"Leer texto {archivo}".center(60,'-'))
+    fic = open(ruta+archivo,'r',encoding='utf8')
+    lines = fic.readlines()
+    lines = [s.rstrip('\n') for s in lines]
+    fic.close()
+    return lines
 
+def crearTxt(nombre:str):
+    print(f"Crear txt {nombre}".center(60,'-'))
+    file=open(ruta+nombre+'.txt','w')
+    file.close()
 
-# print("Abre como nuevo".center(60,'-'))
-# Abienrto en modo escritura
-# nombres = open(ruta,'w')
+def actualizarTxt(file:str,data:list):
+    print(f"Actuaizar txt {file}".center(60,'-'))
+    file=open(ruta+file,'a',encoding='utf8')
+    file.writelines('%s\n' %s for s in data)
+    file.close()
 
+def agregarLineaTxt(file:str,linea:str):
+    print(f"Agregar liena a txt {file}".center(60,'-'))
+    file=open(ruta+file,'a',encoding='utf8')
+    print(linea,file=file)
+    file.close()
 
-# nombres = open(ruta,'r')
-# Abienrto en modo lectura
-# print(nombres.read())
-
-# print("Abre Para añadir texto".center(60,'-'))
-# Abienrto en modo añadir texto
-# nombres = open(ruta,'a')
-print("Escribir texto".center(60,'-'))
-data = ["Línea 1", "Línea 2", "Línea 3", "Línea 4", "Línea 5"]
-fic = open(ruta+'w.txt', "w",encoding='utf8')
-for line in data:
-    fic.write(line)    
-    fic.write("\n")
-fic.close()
-
-print("Leer texto".center(60,'-'))
-fic = open(ruta+'w.txt', "r",encoding='utf8')
-print(fic.read())
-fic.close()
-
-print("Leer linea a linea".center(60,'-'))
-fic = open(ruta+'w.txt', "r",encoding='utf8')
-lines = []
-for line in fic:
-    lines.append(line)
-fic.close()
-
-print(lines)
-lines1 = [s.rstrip('\n') for s in lines]
-print(lines1)
-
-
+leido = leerTxt('w.txt')
+print(leido)
+crearTxt('tareas')
+actualizarTxt('tareas.txt',leido)
+agregarLineaTxt('tareas.txt','una mas')
