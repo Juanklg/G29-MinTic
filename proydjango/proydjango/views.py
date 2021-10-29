382907c3aec0a218064d12993f56a4e766b3922a
Juan Linares <juanlinares.ruta1@utp.edu.co>
2021-10-28 20:05:09 -0500

    Add bootstrap components

--- proydjango/proydjango/views.py
+++ proydjango/proydjango/views.py
@@ -9,28 +9,36 @@ def articulosAdd(req):
     nombre = req.GET['nombre']
     seccion = req.GET['seccion']
     precio = req.GET['precio']
-    diccionario={
-        'nombre' : nombre,
-        'seccion':seccion,
-        'precio':precio,
-    }
     art = articulo.objects.create(nombre=nombre,seccion=seccion,precio=precio)
-    return HttpResponseRedirect('/articulos')
+    msj=f'{nombre} agregado correctamente'
+    return HttpResponseRedirect(f'/articulos/?msj={msj}')
 
+    
 def articulos(req):
-    # art = articulo.objects.filter(seccion='tecnologia')
-    # art = articulo.objects.filter(precio__gt=90)    
-    if(len(req.GET)>=1 and req.GET['articulo']!=''):
-        articulos = articulo.objects.filter(nombre__icontains=req.GET['articulo'])
+    msj = ''
+    conjunto = set()
+    print()
+    if len(req.GET)>=1:
+        if 'search' in req.GET.keys():
+            articulos = articulo.objects.filter(seccion__icontains=req.GET['search']) | articulo.objects.filter(nombre__icontains=req.GET['search'])
+        else:
+            articulos = articulo.objects.all()
+
+        if 'msj' in req.GET.keys():
+            msj = req.GET['msj']
+        else:
+            msj = ''
     else:
         articulos = articulo.objects.all()
+        # filtro de rango de precio
     listArticulos = list(articulos.values())
-    conjunto = set()
     for item in articulos.values():
         conjunto.add(item['seccion'])
+    
     diccionario = {
         'articulos':listArticulos,
-        'secciones':list(conjunto)
+        'secciones':conjunto,
+        'alert':{'tipo':'danger','msj':msj}
     }
     return render(req,'articulos.html',diccionario)
 
@@ -42,7 +50,8 @@ def calculo(req,fechaNacimiento,fechaFutura):
         'fechaNacimiento':fechaNacimiento,
         'edadActual':edadActual,
         'edadFutura':edadFutura,
-        'fechaFutura':fechaFutura
+        'fechaFutura':fechaFutura,
+
     }
     doc_externor=loader.get_template('calculo.html')
     documento=doc_externor.render(diccionario)
@@ -84,3 +93,7 @@ def videos(req):
     doc_externor=loader.get_template('videos.html')
     documento=doc_externor.render(diccionario)    
     return HttpResponse(documento)
+
+
+    # art = articulo.objects.filter(seccion='tecnologia')
+    # art = articulo.objects.filter(precio__gt=90)    
